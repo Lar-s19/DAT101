bd2fc34b63e1e215031a1b6b81684dd8b272782e
Lars <lars.staloy@gmail.com>
2025-10-10 13:13:06 +0200

    task3-1-save

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -2,6 +2,31 @@
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
 
+const text1 = "Gratulerer, du er akkurat gammel nok!";
+const text2 = "Du kan gå på kino!";
+const text3 = "Beklager, du er ikke gammel nok!";
+
+const ageMovie = 16;
+const ageBringAlong = 10;
+let age1 = 19 // Gammel nok til å ta meed seg en person under 16.
+let age2 = 14 // Person er ikke gammel nok til å gå alone.
+
+if(age1 < age2){
+    const age1Old = age1;
+    age1 = age2;
+    age2 = age1Old;
+}
 
 
-
+if (age1 <= ageMovie) { 
+    printOut("Person 1 " + text2);
+    if (age2 >= ageMovie)
+        printOut(text2);
+    else if (age1 >= ageBringAlong){
+        printOut("person 1 " + text1);
+        printOut("Person 2 " + text2);
+    printOut("person 3 " + text3);
+    } 
+}else{
+    printOut("Vi har null case for det!!.");
+}
--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -4,35 +4,69 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
-}
+let wakeUpTime = 8;
+if (wakeUpTime == 7) {
+  printOut("If i wake up at 7, " +"I can take the bus to school.");
+    }else if (wakeUpTime == 8) {
+      printOut("If i wake up at 8, " + "I have to take the train to school.");
+    }else  {
+        printOut("If i wake up later than 8, " + "I have to take a car to school.");      
+    }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number = -1; // Change this value to test the conditions. 
+if (number > 0) {
+  printOut(number + " is Positive");
+} else if (number == 0){
+  printOut("Zero");
+} else{
+  printOut(number + " is Negative");
+}
 printOut(newLine);
-
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 6, 7 ---------------------------------------ss-------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let imageSizeMP = Math.floor(Math.random()*8)+1; // Generates a random number between 0 and 8
+printOut("Image size is " + imageSizeMP + "MP");
+if (imageSizeMP < 4) {
+  printOut("Image is too Small");
+} else if (imageSizeMP > 6) {
+  printOut("Image is too Large");
+} else {
+  printOut("Thank you");
+}
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList =["January", "February", "Mars", "April", "Mai",
+"Jun", "Juli", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+if (monthName.indexOf("r")!==-1) {
+  printOut("It's " + monthName + " You must take vitamin D");
+} else {
+  printOut("It's " + monthName + " You don't need vitamin D");
+}
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+function getDaysInMonth(month) {
+  if (month === "February") {
+    return 28;
+  } else if (
+    month === "April" || month === "June" || month === "September" || month === "November"
+  ) {
+    return 30;
+  } else {
+    return 31;
+  } 
+}
+const monthDays = "Month " + monthName + " has " + getDaysInMonth(monthName) + " days.";  
+printOut(monthDays);
 printOut(newLine);
 
 /* Task 10*/
@@ -40,3 +74,4 @@ printOut("--- Part 10 ----------------------------------------------------------
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
+
